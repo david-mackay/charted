9b83bcf1d5530e0af66aeaff9acf5d809fcd66a4
Maggie Zhang <maggiezhng081@yahoo.com>
2024-05-12 13:14:47 -0400

    Implement google maps after user uploads photos

--- frontend/script.js
+++ frontend/script.js
@@ -35,7 +35,6 @@ function uploadImages() {
 
     // Now we have a FormData object with all the files
     sendImagesToServer(formData);
-
     Array.from(files).forEach(file => {
         var reader = new FileReader();
         reader.onload = function(e) {
@@ -48,6 +47,11 @@ function uploadImages() {
         };
         reader.readAsDataURL(file);
     });
+    // Hide the upload form and show the map area
+    uploadForm.style.display = 'none';
+    // Define the mapArea variable here
+    var mapArea = document.getElementById('map');  // Ensure this ID matches your HTML
+    mapArea.style.display = 'block';  // Show the map area
 }
 
 function sendImagesToServer(formData) {
@@ -55,11 +59,29 @@ function sendImagesToServer(formData) {
         method: 'POST',
         body: formData
     }).then(response => {
-        return response.json();
+        if (!response.ok) { // Checks if the response status code is not in the range 200-299
+            throw new Error('Network response was not ok: ' +  response.statusText);
+        } 
+        return response.json(); // Process the response if it's OK
     }).then(data => {
         console.log('Success:', data);
-        // Optionally handle the response data here
+        // Render maps and handle the response data here
+        initMap();
     }).catch(error => {
         console.error('Error:', error);
     });
+}
+
+async function initMap() {
+    console.log("map called")
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 }, //TODO: return center based on some other info
+        zoom: 8,
+    });
+
+    google.maps.event.trigger(map, 'resize'); // Trigger a resize event after the map has been shown
+    map.setCenter({ lat: -34.397, lng: 150.644 }); // Re-center the map
+    console.log("map returned")
 }
